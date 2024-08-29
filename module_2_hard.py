dde6b7e56c7edfa3be1e07e643fb6a4a9f2b4c11
Sqwonchy <ilianos321@gmail.com>
2024-08-29 19:41:31 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,15 +1,28 @@
 
+# a: int = int(input("Введите целое число от 3 до 20: "))
+# list_ = []
+# for one_num in range(1, a):
+#     two_num = a - 1
+#     while one_num != two_num:
+#         if a % (one_num + two_num) == 0:
+#             list_.append(one_num)
+#             list_.append(two_num)
+#         two_num -= 1
+#
+#
+# print(*list_)
+
+# ВСЕ я исправил чтобы порядок цифр был как у вас
+
 a: int = int(input("Введите целое число от 3 до 20: "))
 list_ = []
 for one_num in range(1, a):
-    two_num = a - 1
-    while one_num != two_num:
+    two_num = one_num + 1
+    while two_num != a:
         if a % (one_num + two_num) == 0:
             list_.append(one_num)
             list_.append(two_num)
-        two_num -= 1
-
-
+        two_num += 1
 print(*list_)
 
 
@@ -19,21 +32,3 @@ print(*list_)
 
 
 
-
-
-
-# по другом хотел пойти.
-# a: int = 20
-# list_ = []
-# for all_numb in range(1, a):
-#     number = all_numb + 1
-#     for numb in range(number,a):
-#         if number == 1:
-#             continue
-#         elif a % (all_numb + numb):
-#             list_.append(all_numb)
-#             list_.append(numb)
-#         else:
-#             continue
-#
-# print(*list_, sep=" ")
